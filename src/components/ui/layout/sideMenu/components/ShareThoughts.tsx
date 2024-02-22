73eb59e29276adec191b6d8a4d6fb557901b9142
BekaDvali132 <beka.dvali2001@gmail.com>
2024-02-22 14:13:59 +0400

    add: share thought in sidemenu

---
+++ src/components/ui/layout/sideMenu/components/ShareThoughts.tsx
@@ -0,0 +1,27 @@
+/// <reference types="vite-plugin-svgr/client" />
+import { JSX } from 'react';
+// eslint-disable-next-line import/no-unresolved
+import Bulb from '../../../../../assets/svgs/lamp.svg?react';
+
+export default function ShareThoughts():JSX.Element {
+  return (
+    <div className="mt-auto px-[30px] py-[25px] flex flex-col items-center">
+      <div className="h-[200px] w-full rounded-lg share-thoughts">
+        <div className="share-thougths-bulb">
+          <Bulb />
+          <div className="shadow" />
+        </div>
+        <div className="flex flex-col gap-2 items-center mt-3">
+          <p className="text-sm">Thoughts Time</p>
+          <p className="text-sm text-center">We don’t have any notice for you, till then you can share your thoughts with your peers.</p>
+          <button
+            type="button"
+            className="text-sm bg-white border-none py-3 px-[27px] rounded-lg"
+          >
+            Thoughts Time
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
