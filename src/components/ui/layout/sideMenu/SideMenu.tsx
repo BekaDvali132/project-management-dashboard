af0bf4c85d64806b2ac1f819a83f335d916f0254
BekaDvali132 <beka.dvali2001@gmail.com>
2024-02-26 13:10:22 +0400

    add: mantine ui

--- src/components/ui/layout/sideMenu/SideMenu.tsx
+++ src/components/ui/layout/sideMenu/SideMenu.tsx
@@ -6,11 +6,11 @@ import ShareThoughts from './components/ShareThoughts.tsx';
 function SideMenu(): JSX.Element {
   return (
     <div className="flex flex-col w-64 border-solid border-r-[1px] border-[#DBDBDB] gap-[30px] h-screen">
-      <div className="flex justify-center items-center h-[88px] border-solid border-b-[1px] border-[#DBDBDB] hover:bg-[rgba(80, 48, 229, 0.08)]">
+      <div className="flex justify-center items-center min-h-[88px] border-solid border-b-[1px] border-[#DBDBDB] hover:bg-[rgba(80, 48, 229, 0.08)]">
         <p>Project Mng.</p>
       </div>
       <NavList />
-      <div className="w-full h-[1px] bg-[#DBDBDB]" />
+      <div className="h-[1px] bg-[#DBDBDB] mx-3" />
       <ProjectsList />
       <ShareThoughts />
     </div>
