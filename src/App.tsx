4d67ecb7a2a58f042636d5e842678102925ecaef
BekaDvali132 <beka.dvali2001@gmail.com>
2024-02-28 17:14:30 +0400

    add: react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/ui/layout/Layout.tsx';
+import Error from './pages/Error.tsx';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <Error />,
+    },
+  ]);
   return (
-    <Layout>
-      <div />
-    </Layout>
+    <RouterProvider router={router} />
   );
 }
 
--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,19 +1,16 @@
 import { JSX } from 'react';
+import { Outlet } from 'react-router-dom';
 import Header from './header/Header.tsx';
 import SideMenu from './sideMenu/SideMenu.tsx';
 
-interface LayoutProps {
-    children: JSX.Element;
-}
-
-export default function Layout({ children }: LayoutProps): JSX.Element {
+export default function Layout(): JSX.Element {
   return (
     <div className="flex min-h-screen">
       <SideMenu />
       <div className="flex flex-col flex-1">
         <Header />
         <main className="flex-grow">
-          {children}
+          <Outlet />
         </main>
       </div>
     </div>
